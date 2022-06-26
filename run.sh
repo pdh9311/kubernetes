21810c99353d47a4535006214d1ab839964e6cf6
pdh9311 <padohy@gmail.com>
2022-06-27 04:53:26 +0900

    kuber

--- run.sh
+++ run.sh
@@ -6,3 +6,15 @@ if [ $# -ne 0 ]; then
 	./static_ip.sh $1
 	chmod 777 /var/run/docker.sock
 fi
+
+# Swap disabled
+swapoff -a && sed -i '/swap/s/^/#/' /etc/fstab
+
+# kubeadm, kubelet, kubectl 설치
+apt update
+apt install -y apt-transport-https ca-certificates curl
+curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
+apt update
+apt install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
