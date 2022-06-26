7467211472b304a81df8a22f089cdff57b5ed6fd
pdh9311 <padohy@gmail.com>
2022-06-27 06:58:48 +0900

    kuber

--- run.sh
+++ run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+## Color
+CYAN="\033[96m"
+NC="\033[0m"
+
 ## Docker install
-# CHECK_DOCKER=$(docker version | grep "Docker Engine - Community" | wc -l)
 if [ ! -f "/var/run/docker.sock" ]; then
+    echo -e "$CYAN Docker install $NC"
     curl -fsSL https://get.docker.com -o get-docker.sh
     sh get-docker.sh
     chmod 777 /var/run/docker.sock
@@ -14,12 +18,14 @@ fi
 # Swap disabled
 CHECK_SWAP=$(free -h | grep "Swap" | awk '{print $2}')
 if [ $CHECK_SWAP != "0B" ]; then
+    echo -e "$CYAN Swap disabled $NC"
     swapoff -a && sed -i '/swap/s/^/#/' /etc/fstab
 fi
 
-# kubeadm, kubelet, kubectl 설치
+# (kubeadm, kubelet, kubectl) install
 CHECK_KUBE=$(dpkg -l | grep kubectl | wc -l)
 if [ $CHECK_KUBE -eq 0 ]; then
+    echo -e "$CYAN (kubeadm, kubelet, kubectl) install $NC"
     apt update
     apt install -y apt-transport-https ca-certificates curl
     curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
@@ -29,7 +35,7 @@ if [ $CHECK_KUBE -eq 0 ]; then
     apt-mark hold kubelet kubeadm kubectl
 fi
 
-## static ip setting
+## Static IP Setting
 HOSTNAME=$(hostname)
 CURRENT_IP=$(ip addr | grep "inet.*enp0s3" | awk '{print $2}')
 IP_RANGE=$(echo `expr "$CURRENT_IP" : '\([0-9]*\.[0-9]*.[0-9]*.\)'`)
@@ -48,6 +54,7 @@ IP=$IP_RANGE$IP_LAST_BIT$SUBNETMASK
 GATEWAY=$IP_RANGE"1"
 CHECK_STATIC_IP=$(cat /etc/netplan/00-installer-config.yaml | grep "addresses:" | wc -l)
 if [ $CHECK_STATIC_IP -eq 0 ]; then
+echo -e "$CYAN Static IP Setting $NC"
 echo "network:
   ethernets:
     enp0s3:
