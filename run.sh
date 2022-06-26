bca8059531477328b6b254c40e9532b31fbb4b50
pdh9311 <padohy@gmail.com>
2022-06-27 05:17:07 +0900

    kuber

--- run.sh
+++ run.sh
@@ -8,13 +8,19 @@ if [ $# -ne 0 ]; then
 fi
 
 # Swap disabled
-swapoff -a && sed -i '/swap/s/^/#/' /etc/fstab
+SWAP=$(free -h | grep "Swap" | awk '{print $2}')
+if [ $SWAP -ne "0B" ]; then
+	swapoff -a && sed -i '/swap/s/^/#/' /etc/fstab
+fi
 
 # kubeadm, kubelet, kubectl 설치
-apt update
-apt install -y apt-transport-https ca-certificates curl
-curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
-apt update
-apt install -y kubelet kubeadm kubectl
-apt-mark hold kubelet kubeadm kubectl
+KUBE=$(dpkg -l | grep kubectl | wc -l)
+if [ $KUBE -eq 0 ]; then
+	apt update
+	apt install -y apt-transport-https ca-certificates curl
+	curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+	echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
+	apt update
+	apt install -y kubelet kubeadm kubectl
+	apt-mark hold kubelet kubeadm kubectl
+fi
