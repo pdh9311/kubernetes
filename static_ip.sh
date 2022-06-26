babce04fab98852c02b7332473eb389fedfddcc7
pdh9311 <padohy@gmail.com>
2022-06-26 15:05:50 +0900

    kuber

--- static_ip.sh
+++ static_ip.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # static ip setting
+echo -e "\033[96m./static_ip.sh [ip_last_bit]\033[0m"
+
+if [ $# -ne 0 ]; then
 CURRENT_IP=$(ip addr | grep "inet.*enp0s3" | awk '{print $2}')
 IP_RANGE=$(echo `expr "$CURRENT_IP" : '\([0-9]*\.[0-9]*.[0-9]*.\)'`)
-IP_LAST_BIT="60"
+IP_LAST_BIT=$1
 SUBNETMASK="/24"
 IP=$IP_RANGE$IP_LAST_BIT$SUBNETMASK
 GATEWAY=$IP_RANGE"1"
@@ -15,10 +18,12 @@ echo "network:
       addresses: [$IP]
       routes:
         - to: default
-	  via: $GATEWAY
+          via: $GATEWAY
       nameservers:
         addresses: [8.8.8.8, 8.8.4.4, $GATEWAY]
   version: 2" \
-	  > /etc/netplan/00-installer-config.yaml
+          > /etc/netplan/00-installer-config.yaml
 
 netplan apply
+
+fi
