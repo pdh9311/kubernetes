8764b3ae0ead50c02cb2e2eac17adc1d04766529
pdh9311 <padohy@gmail.com>
2022-06-27 05:56:35 +0900

    kuber

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 
-echo -e "\033[96m./run [last_ip_bit]\033[0m"
+# echo -e "\033[96m./run [last_ip_bit]\033[0m"
 
 ## Docker install
-DOCKER=$(docker version | grep "Docker Engine - Community" | wc -l)
-if [ $DOCKER -eq 0 ]; then
+CHECK_DOCKER=$(docker version | grep "Docker Engine - Community" | wc -l)
+if [ $CHECK_DOCKER -eq 0 ]; then
     curl -fsSL https://get.docker.com -o get-docker.sh
     sh get-docker.sh
     chmod 777 /var/run/docker.sock
@@ -15,14 +15,14 @@ fi
 
 ## Kubernetes install
 # Swap disabled
-SWAP=$(free -h | grep "Swap" | awk '{print $2}')
-if [ $SWAP != "0B" ]; then
+CHECK_SWAP=$(free -h | grep "Swap" | awk '{print $2}')
+if [ $CHECK_SWAP != "0B" ]; then
 	swapoff -a && sed -i '/swap/s/^/#/' /etc/fstab
 fi
 
 # kubeadm, kubelet, kubectl 설치
-KUBE=$(dpkg -l | grep kubectl | wc -l)
-if [ $KUBE -eq 0 ]; then
+CHECK_KUBE=$(dpkg -l | grep kubectl | wc -l)
+if [ $CHECK_KUBE -eq 0 ]; then
 	apt update
 	apt install -y apt-transport-https ca-certificates curl
 	curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
@@ -33,26 +33,35 @@ if [ $KUBE -eq 0 ]; then
 fi
 
 ## static ip setting
-if [ $# -ne 0 ]; then
+HOSTNAME=$(hostname)
 CURRENT_IP=$(ip addr | grep "inet.*enp0s3" | awk '{print $2}')
 IP_RANGE=$(echo `expr "$CURRENT_IP" : '\([0-9]*\.[0-9]*.[0-9]*.\)'`)
-IP_LAST_BIT=$1
+if [ $HOSTNAME == "k8s-master" ]; then
+	IP_LAST_BIT="56"
+elif [ $HOSTNAME == "k8s-node1" ]; then
+	IP_LAST_BIT="57"
+elif [ $HOSTNAME == "k8s-node2" ]; then
+	IP_LAST_BIT="60"
+else
+	IP_LAST_BIT=$1
+fi
 SUBNETMASK="/24"
 IP=$IP_RANGE$IP_LAST_BIT$SUBNETMASK
 GATEWAY=$IP_RANGE"1"
+CHECK_STATIC_IP=$(cat /etc/netplan/\*.yaml | grep "addresses:" | wc -l)
+if [ $CHECK_STATIC_IP -eq 0 ]; then
+	echo "network:
+	ethernets:
+		enp0s3:
+		dhcp4: false
+		addresses: [$IP]
+		routes:
+			- to: default
+			via: $GATEWAY
+		nameservers:
+			addresses: [8.8.8.8, 8.8.4.4, $GATEWAY]
+	version: 2" \
+			> /etc/netplan/00-installer-config.yaml
 
-echo "network:
-  ethernets:
-    enp0s3:
-      dhcp4: false
-      addresses: [$IP]
-      routes:
-        - to: default
-          via: $GATEWAY
-      nameservers:
-        addresses: [8.8.8.8, 8.8.4.4, $GATEWAY]
-  version: 2" \
-          > /etc/netplan/00-installer-config.yaml
-
-netplan apply
+	netplan apply
 fi
