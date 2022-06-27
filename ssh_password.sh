8e461155be288dfc79a5c7829282b896d2713984
pdh9311 <padohy@gmail.com>
2022-06-27 18:13:59 +0900

    kuber

--- ssh_password.sh
+++ ssh_password.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+ROOT_PW=qwe123
+echo "root:$ROOT_PW" | chpasswd
+
 ## ssh password 접속 활성화
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 sed -i 's/#PermitRootLogin yes/PermitRootLogin yes/g' /etc/ssh/sshd_config
